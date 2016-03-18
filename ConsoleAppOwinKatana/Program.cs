e6e5d9ddfd933cfb4ac125f277816540a89fa1d7
millom <mikael.lundstrom@studentconsulting.com>
2016-03-18 16:16:00 +0100

    First commit

---
+++ ConsoleAppOwinKatana/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppOwinKatana
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string url = "http://localhost:8080";
+
+			using (WebApp.Start<Startup>(url))
+			{
+				Console.WriteLine("Started");
+				Console.ReadKey();
+				Console.WriteLine("Stopping");
+			}
+		}
+	}
+
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			app.Use<HelloWorldComponent>();
+
+			//app.UseWelcomePage();
+
+			//app.Run(ctx =>
+			//	{
+			//		return ctx.Response.WriteAsync("Hello World");
+			//	});
+		}
+	}
+
+	public class HelloWorldComponent
+	{
+		public Task Inwoke(IDictionary<string, object> enviroment)
+		{
+			return null;
+		}
+	}
+}
